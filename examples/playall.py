2ed85f59104cb5e45c9586ccd2196763faf52652
b3b <ash.b3b@gmail.com>
2015-03-31 02:18:02 +0300

    add "PlayAll" example

---
+++ examples/playall.py
@@ -0,0 +1,52 @@
+'''Sequentially play all the MIDI instruments.
+'''
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty, StringProperty
+from kivy.clock import Clock
+from itertools import cycle
+from midistream import Midi
+from instrument.helpers import midi_note_on, midi_note_off, midi_program_change, midi_instruments
+
+
+class MainLayout(BoxLayout):
+    pass
+
+
+class PlayAll(App):
+    instrument = StringProperty('')
+    note = NumericProperty(0)
+    start_note = 60
+    end_note = 64
+    note_step = 2
+
+    def build(self):
+        return MainLayout()
+
+    def on_start(self):
+        self.midi = Midi()
+        self.midi.start()
+
+        self.instruments = cycle(midi_instruments.items())
+        Clock.schedule_interval(self.change_note, 1)
+
+    def change_note(self, *args):
+        if self.note:
+            self.midi.write_short(*midi_note_off(self.note))
+            self.note += self.note_step
+            if self.note > self.end_note:
+                self.change_instrument()
+        else:
+            self.change_instrument()
+        self.midi.write_short(*midi_note_on(self.note))
+
+    def change_instrument(self, *args):
+        program, title = next(self.instruments)
+        self.midi.write_short(*midi_program_change(program))
+        self.instrument = title
+        self.note = self.start_note
+
+
+if __name__ == '__main__':
+    PlayAll().run()
