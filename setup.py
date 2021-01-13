edf8f2c00862f5d10e7cee370813de9d190fb887
b3b <ash.b3b@gmail.com>
2021-01-13 22:30:30 +0300

    switch to setup.py based build

--- setup.py
+++ setup.py
@@ -5,17 +5,26 @@ try:
     from Cython.Distutils import build_ext
 except ImportError:
     from distutils.command.build_ext import build_ext
+
     sources = ["midi.c"]
 else:
     sources = ["midi.pyx"]
 
-
 setup(
     name="midistream",
     packages=["midistream"],
-    package_dir={"midistream": "."},
     cmdclass={"build_ext": build_ext},
     ext_modules=[
-        Extension("midistream.midi", sources=sources),
+        Extension(
+            "libsonivox",
+            sources=["sonivox.c"],
+            extra_link_args=["-o", "./libsonivox.so"],
+        ),
+        Extension(
+            "midistream.midi",
+            sources=sources,
+            libraries=["sonivox"],
+            extra_link_args=["-L", "."],
+        ),
     ],
 )
